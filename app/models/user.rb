cf4993207f6d4b4f1f04a54c4a584a8ff416be3b
pkhilenko <pkhilenko@yandex.ru>
2018-12-04 14:13:03 +0600

    not_save_reserved_data_from_services

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
                                 message: "Только русские буквы и цифры, начинаться должно с буквы" }                               
   validates :nikname, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
+
+  before_save :not_save_reserved_data_from_services
+
+  private
+
+  def not_save_reserved_data_from_services
+    throw :abort if (nikname == 'testtesttesttest') || (email == 'test@test.test')
+  end
 end
